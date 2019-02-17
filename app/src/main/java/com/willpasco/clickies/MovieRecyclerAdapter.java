c783b58a86b5b1923e6638a98b45dfec1a1517fc
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-17 13:28:55 -0300

    Creating recycler adapter.

---
+++ app/src/main/java/com/willpasco/clickies/MovieRecyclerAdapter.java
@@ -0,0 +1,43 @@
+package com.willpasco.clickies;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MovieRecyclerAdapter extends RecyclerView.Adapter<MovieRecyclerAdapter.MovieViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position)
+    {
+        return new MovieViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.moview_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position)
+    {
+        holder.onBind();
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return 0;
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        MovieViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+        }
+
+        void onBind(){
+
+        }
+
+    }
+}
