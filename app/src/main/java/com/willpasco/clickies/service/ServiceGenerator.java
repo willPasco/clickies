db8f59c91b4e97163572b133631c3509de181d70
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-17 14:46:14 -0300

    Initiating retrofit service.

---
+++ app/src/main/java/com/willpasco/clickies/service/ServiceGenerator.java
@@ -0,0 +1,35 @@
+package com.willpasco.clickies.service;
+
+import com.google.gson.Gson;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    public static final String BASE_URL = "https://community-neutrino-currency-conversion.p.mashape.com/";
+
+    public static <S> S createService(Class<S> serviceClass) {
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .readTimeout(15, TimeUnit.SECONDS);
+
+        httpClient.addInterceptor(loggingInterceptor);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .client(httpClient.build())
+                .build();
+
+        return retrofit.create(serviceClass);
+    }
+}
