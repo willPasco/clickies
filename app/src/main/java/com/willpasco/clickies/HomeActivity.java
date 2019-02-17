6702f1cb8ca5b33895b7c825749882c0ceb9d13d
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-17 13:38:27 -0300

    Implementing adapter.

--- app/src/main/java/com/willpasco/clickies/HomeActivity.java
+++ app/src/main/java/com/willpasco/clickies/HomeActivity.java
@@ -2,6 +2,9 @@ package com.willpasco.clickies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class HomeActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        MovieRecyclerAdapter adapter = new MovieRecyclerAdapter();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
