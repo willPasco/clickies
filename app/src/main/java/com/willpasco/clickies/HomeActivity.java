99bd0fb9b9d97257763824510c07fd6eb1df405d
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-17 16:19:53 -0300

    Testing response from api.

--- app/src/main/java/com/willpasco/clickies/HomeActivity.java
+++ app/src/main/java/com/willpasco/clickies/HomeActivity.java
@@ -1,26 +1,60 @@
 package com.willpasco.clickies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.LinearLayout;
+import android.util.Log;
 
-import butterknife.BindView;
+import com.willpasco.clickies.model.MovieJsonResponse;
+import com.willpasco.clickies.service.RetrofitService;
+import com.willpasco.clickies.service.ServiceGenerator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.willpasco.clickies.service.ServiceGenerator.API_KEY;
 
 public class HomeActivity extends AppCompatActivity {
-    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    private static final String TAG = "HomeActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         MovieRecyclerAdapter adapter = new MovieRecyclerAdapter();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this, LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this, RecyclerView.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+
+        retrofitConverter("popular");
+    }
+
+
+    public void retrofitConverter(String order) {
+
+        RetrofitService service = ServiceGenerator.createService(RetrofitService.class);
+
+        Call<MovieJsonResponse> call = service.getMovies(order, API_KEY);
+
+        call.enqueue(new Callback<MovieJsonResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<MovieJsonResponse> call, @NonNull Response<MovieJsonResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.i(TAG, response.body() + "");
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<MovieJsonResponse> call, @NonNull Throwable t) {
+
+            }
+        });
+
     }
 }
