e3ccef6cd227621286a751619321e3e3108d2b1d
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-17 14:45:29 -0300

    Creating pojos to api response.

---
+++ app/src/main/java/com/willpasco/clickies/model/MovieJsonResponse.java
@@ -0,0 +1,55 @@
+package com.willpasco.clickies.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class MovieJsonResponse {
+
+    @SerializedName("page")
+    @Expose
+    private Integer page;
+    @SerializedName("total_results")
+    @Expose
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    @Expose
+    private Integer totalPages;
+    @SerializedName("results")
+    @Expose
+    private List<Movie> movieList = null;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Movie> getResults() {
+        return movieList;
+    }
+
+    public void setResults(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+}
