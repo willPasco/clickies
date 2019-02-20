6a34ddd9771272f091c78b07e572430e3eac716b
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-02-19 22:55:29 -0300

    Improving image content.

--- app/src/main/java/com/willpasco/clickies/util/ImageLoader.java
+++ app/src/main/java/com/willpasco/clickies/util/ImageLoader.java
@@ -3,14 +3,17 @@ package com.willpasco.clickies.util;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 
 public class ImageLoader {
 
     public static void loadImage(String url, ImageView view) {
 
+
         Glide.with(view.getContext())
                 .load(url)
-                .fitCenter()
+                .transform(new CenterCrop(), new RoundedCorners(20))
                 .into(view);
     }
 }
