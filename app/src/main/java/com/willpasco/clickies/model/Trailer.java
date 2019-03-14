4904eb81533fc3351962eaca8b2741ab0e541fdb
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-03-14 17:05:09 -0300

    Creating trailer pojos.

---
+++ app/src/main/java/com/willpasco/clickies/model/Trailer.java
@@ -0,0 +1,60 @@
+package com.willpasco.clickies.model;
+
+public class Trailer {
+
+    private String id;
+    private String key;
+    private String name;
+    private String site;
+    private Integer size;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
