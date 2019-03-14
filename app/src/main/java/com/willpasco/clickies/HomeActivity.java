bbb54c51957d567e1243352fde6a05ff0e279dfe
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-03-14 12:46:57 -0300

    Setting the different states.

--- app/src/main/java/com/willpasco/clickies/HomeActivity.java
+++ app/src/main/java/com/willpasco/clickies/HomeActivity.java
@@ -1,13 +1,17 @@
 package com.willpasco.clickies;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.nightonke.jellytogglebutton.JellyToggleButton;
 import com.nightonke.jellytogglebutton.State;
 import com.willpasco.clickies.model.Movie;
@@ -16,11 +20,13 @@ import com.willpasco.clickies.viewmodel.MovieViewModel;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -28,14 +34,16 @@ public class HomeActivity extends AppCompatActivity {
     public static final String TOP_RATED_SEARCH_TYPE = "top_rated";
     private static final int CONNECTION_CHECK_REQUEST_CODE = 4521;
 
+    private CoordinatorLayout rootView;
     private MovieRecyclerAdapter adapter;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
     private JellyToggleButton jellyToggleButton;
-    private LinearLayout errorState;
+    private LinearLayout noContentState;
     private LinearLayout noNetWorkState;
     private String searchType = POPULAR_SEARCH_TYPE;
     private MovieViewModel movieViewModel;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +53,15 @@ public class HomeActivity extends AppCompatActivity {
         movieViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
 
         onBindView();
-//        configErrorButton();
-        configNoNetworkButton();
 
         adapter = new MovieRecyclerAdapter();
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshData();
+            }
+        });
 
         jellyToggleButton.setOnStateChangeListener(new JellyToggleButton.OnStateChangeListener() {
             @Override
@@ -57,15 +69,16 @@ public class HomeActivity extends AppCompatActivity {
                 if (state == State.LEFT) {
                     showLoadingState();
                     searchType = POPULAR_SEARCH_TYPE;
-                    movieViewModel.getListMovieLiveData(searchType);
+                    refreshData();
                 } else if (state == State.RIGHT) {
                     showLoadingState();
                     searchType = TOP_RATED_SEARCH_TYPE;
-                    movieViewModel.getListMovieLiveData(searchType);
+                    refreshData();
                 }
             }
         });
 
+        refreshData();
 
         GridLayoutManager layoutManager = new GridLayoutManager(HomeActivity.this, 2);
         recyclerView.setLayoutManager(layoutManager);
@@ -77,139 +90,92 @@ public class HomeActivity extends AppCompatActivity {
             public void onChanged(List<Movie> movies) {
                 showContentState();
                 adapter.addAll(movies);
+
+                dismissRefreshing();
+
                 if(adapter.getItemCount() <= 0){
-                    showErrorState();
+                    showNoContentState();
                 }
             }
         });
 
-//        requestMovies(searchType);
     }
 
-/*
+    private void dismissRefreshing() {
+        if(swipeRefreshLayout.isRefreshing()){
+            swipeRefreshLayout.setRefreshing(false);
+        }
+    }
+
+    private void refreshData() {
+        if(!isNetworkAvailable()){
+            dismissRefreshing();
+            showNoNetworkState();
+        }else{
+            movieViewModel.getListMovieLiveData(searchType);
+        }
+    }
+
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 
-
-
-    private void requestMovies(String order) {
-        if (isNetworkAvailable()) {
-
-            RetrofitService service = ServiceGenerator.createService(RetrofitService.class);
-
-            Call<MovieJsonResponse> call = service.getMovies(order, API_KEY);
-
-            call.enqueue(new Callback<MovieJsonResponse>() {
-                @Override
-                public void onResponse(@NonNull Call<MovieJsonResponse> call, @NonNull Response<MovieJsonResponse> response) {
-                    if (response.isSuccessful()) {
-                        if (response.body() != null) {
-                            recyclerView.scrollToPosition(0);
-                            adapter.addAll(response.body().getResults());
-                            showContentState();
-                        } else {
-                            showErrorState();
-                        }
-                    } else {
-                        showErrorState();
-                    }
-                }
-
-                @Override
-                public void onFailure(@NonNull Call<MovieJsonResponse> call, @NonNull Throwable t) {
-                    showErrorState();
-                }
-            });
-
-        } else {
-            showNoNetworkState();
-        }
-
-    }
-*/
-
     private void onBindView() {
         recyclerView = findViewById(R.id.recycler_view);
         progressBar = findViewById(R.id.progress_bar);
         jellyToggleButton = findViewById(R.id.jellyToggleButton);
-        errorState = findViewById(R.id.include_error_state);
+        noContentState = findViewById(R.id.include_no_content_state);
         noNetWorkState = findViewById(R.id.include_no_network_state);
+        rootView = findViewById(R.id.parent_layout);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
     }
 
 
-/*
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CONNECTION_CHECK_REQUEST_CODE) {
             if (!isNetworkAvailable()) {
                 showNoNetworkState();
-                Toast.makeText(HomeActivity.this, R.string.toast_no_internet_connection, Toast.LENGTH_LONG).show();
             } else {
-                requestMovies(searchType);
+                Toast.makeText(this, R.string.internet_connected, Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private void configErrorButton() {
-        Button retryButton = errorState.findViewById(R.id.button_retry);
 
-        retryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showLoadingState();
-                requestMovies(searchType);
-            }
-        });
-    }
-*/
-
-    private void configNoNetworkButton() {
-        Button buttonConfig = noNetWorkState.findViewById(R.id.button_config);
-
-        buttonConfig.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showLoadingState();
-                Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
-                startActivityForResult(intent, HomeActivity.CONNECTION_CHECK_REQUEST_CODE);
-            }
-        });
-    }
-
-
-    private void showErrorState() {
+    private void showNoContentState() {
         recyclerView.setVisibility(View.GONE);
         progressBar.setVisibility(View.GONE);
-        errorState.setVisibility(View.VISIBLE);
-        noNetWorkState.setVisibility(View.GONE);
+        noContentState.setVisibility(View.VISIBLE);
     }
 
 
     private void showContentState() {
         progressBar.setVisibility(View.GONE);
         recyclerView.setVisibility(View.VISIBLE);
-        errorState.setVisibility(View.GONE);
-        noNetWorkState.setVisibility(View.GONE);
+        noContentState.setVisibility(View.GONE);
     }
 
 
     private void showLoadingState() {
         recyclerView.setVisibility(View.GONE);
         progressBar.setVisibility(View.VISIBLE);
-        errorState.setVisibility(View.GONE);
-        noNetWorkState.setVisibility(View.GONE);
+        noContentState.setVisibility(View.GONE);
     }
 
     private void showNoNetworkState() {
-        recyclerView.setVisibility(View.GONE);
-        progressBar.setVisibility(View.GONE);
-        errorState.setVisibility(View.GONE);
-        noNetWorkState.setVisibility(View.VISIBLE);
-
+        Snackbar.make(recyclerView, R.string.no_network, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.snack_bar_settings_action, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Settings.ACTION_WIRELESS_SETTINGS);
+                        startActivityForResult(intent, HomeActivity.CONNECTION_CHECK_REQUEST_CODE);
+                    }
+                }).setActionTextColor(getResources().getColor(R.color.snack_action_color))
+                .show();
     }
 
 }
